1dc9dc5d783c77dbe7ec6463a0dc04a69283b8f8
Mehdi Seyedain <mahdi.seyedain@gmail.com>
2020-12-07 13:27:51 +0330

    Create 08_01.js

---
+++ 08_01.js
@@ -0,0 +1,11 @@
+function reliableMultiply(a, b){
+	for(;;){
+		try{
+			return primitiveMultiply(a, b);
+		}
+		catch(error){
+			if(!error instanceof MultiplicatorUnitFailure)
+				throw error;
+		}	
+	}
+}
